9a1684a55209edbc3319053a56c96b2116d4e7d8
Acilya <alicjakapiszka@gmail.com>
2017-12-02 14:10:38 +0100

    Add files via upload

--- mo/L3/met_gradientu_prostego.m
+++ mo/L3/met_gradientu_prostego.m
@@ -1,7 +1,7 @@
 v = -5:0.1:5;
-lambda = 0.2;
-h = 0.01;
-epsilon = 0.01;
+lambda = 0.1;
+h = 0.1;
+epsilon = 0.2;
 [x, y] = meshgrid(v);
 f = '2*x.^2+y.^2+x.*y-6*x-5*y+8';
 z = 2*x.^2+y.^2+x.*y-6*x-5*y+8;
@@ -13,17 +13,24 @@ hold on
 
 x0 = -5;
 y0 = -5;
-xn = [x0; y0];
+xxn = [x0; y0];
+xn = x0;
 yn = y0;
 
 while true
-    xn_stale = xn;
-    if norm([(F(xn+h,yn)-F(xn, yn))/h; (F(xn,yn+h)-F(xn,yn))/h]) < epsilon
+    grad1 = (F(xn+h, yn) - F(xn, yn)) / h;
+    grad2 = (F(xn, yn+h) - F(xn, yn)) / h;
+    Grad = [grad1; grad2];
+    if norm(Grad) < epsilon
         break;
     else
-        grad1 = (F(xn+h, yn) - F(xn, yn)) / h;
-        grad2 = (F(xn, yn+h) - F(xn, yn)) / h;
-        grad = [grad1; grad2];
-        xn = xn - (grad / norm(grad)) * lambda;
+        xxn = xxn - (Grad / norm(Grad)) * lambda;
+        x0 = xn;
+        y0 = yn;
+        xn = xxn(1,1);
+        yn = xxn(2,1);
     end
+    plot([x0 xn], [y0 yn], '-')
+    hold on;
 end
+[xn yn]
